b3125b181f0702bc2aeb0a091d76df9012b5ff20
dead-shadow-7 <deadshadow12119@gmail.com>
2024-12-17 15:21:55 +0530

    Updated the status bar color and navigation color

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 import 'pages/login_page.dart';
 import 'pages/start.dart';
 
-void main()  {
-  
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor: Color(0xFF0C081E), // navigation bar color
+    statusBarColor: Color(0xFF0C081E),
+    statusBarIconBrightness: Brightness.light,
+  ));
   runApp(const MyApp());
 }
 
@@ -12,11 +16,10 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-    Widget build (BuildContext context)  {
+  Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:   StartPage(),
-      );
-      
+      home: StartPage(),
+    );
   }
 }
